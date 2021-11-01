57d3b6c8da7736c59b6afa6ad2a2d23e006d34f2
SimbaUni <simon_love@live.co.uk>
2021-11-01 01:00:03 +0000

    Updated homepage, populated with api and styling

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="hero">
-      <img src="../assets/imgs/filmHero.jpg" alt="" />
+      <img src="../assets/imgs/movieHero.jpg" alt="" />
       <div class="text-container">
           <div class="text">
               <span class="mini-heading">Now Streaming</span>
               <h1><span>Now</span>Streaming</h1>
-              <a href="#film-grid" class="button">View Films</a>
+              <a href="#movie-grid" class="button">View Movies</a>
           </div>
       </div>
   </div>
